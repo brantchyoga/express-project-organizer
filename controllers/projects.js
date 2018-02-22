4d8a69d625882c77c9a8ab5a8a9e7c7bbb14e52c
brantchyoga <brantrporter@gmail.com>
2018-02-22 00:16:10 -0800

    to late to code

--- controllers/projects.js
+++ controllers/projects.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var db = require('../models');
+var async = require('async');
+var bodyParser = require('body-parser');
 var router = express.Router();
+
 
 // POST /projects - create a new project
 router.post('/', function(req, res) {
+  var categories = [];
+  if(req.body.categories){
+    categories = req.body.categories.split(",");
+  }
   db.project.create({
     name: req.body.name,
     githubLink: req.body.githubLink,
@@ -11,9 +18,22 @@ router.post('/', function(req, res) {
     description: req.body.description
   })
   .then(function(project) {
-    res.redirect('/');
+    if(categories.length > 0){
+      async.forEach(categories, function(category, callback){
+        db.category.findOrCreate({
+          where: {name: category.trim()}
+        }).spread(function(newCategory, wasCreated){
+          callback();
+        })
+      })
+    }, function(){
+      res.redirect('/projects/'+project.id);
+    } else {
+      res.redirect('/projects/'+project.id);
+    }
   })
   .catch(function(error) {
+    console.log(error)
     res.status(400).render('main/404');
   });
 });
@@ -26,13 +46,15 @@ router.get('/new', function(req, res) {
 // GET /projects/:id - display a specific project
 router.get('/:id', function(req, res) {
   db.project.find({
-    where: { id: req.params.id }
+    where: { id: req.params.id },
+    include: [db.category]
   })
   .then(function(project) {
     if (!project) throw Error();
     res.render('projects/show', { project: project });
   })
   .catch(function(error) {
+    console.log(error)
     res.status(400).render('main/404');
   });
 });
